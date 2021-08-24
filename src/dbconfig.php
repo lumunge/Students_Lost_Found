d367c66de2fff6b50f76523d1137f982591204ad
Erick Lumunge <lumungep12@gmail.com>
2021-08-24 20:27:50 +0300

    File restructure

--- src/dbconfig.php
+++ src/dbconfig.php
@@ -1,15 +1,10 @@
 
 <?php
 /* Attempt to connect to MySQL database */
-$conn = mysqli_connect("localhost", "root", "", "studentid");
+$conn = mysqli_connect("localhost", "root", "", "students_id");
  
 // Check connection
 if($conn === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
 ?>
-
-
-
-
-
